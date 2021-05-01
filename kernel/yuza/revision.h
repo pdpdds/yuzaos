6f79b5d3a56246840fce37a38bdf26fb2018b7a7
Juhang Park <juhang3@daum.net>
2021-05-02 03:52:44 +0900

    solution configuration

--- kernel/yuza/revision.h
+++ kernel/yuza/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "19 April 2021"
-#define BUILD_TIME "03:41:01"
+#define BUILD_DATE "02 May 2021"
+#define BUILD_TIME "03:49:17"
 #define BUILD_SYSTEM "msvc"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 11312
+#define REVISION_BUILD 11316
 
 #endif //!_REVISION_H_
