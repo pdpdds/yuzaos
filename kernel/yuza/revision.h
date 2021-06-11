01901de5351072866debf5f735316c6b336b2b3c
Juhang Park <juhang3@daum.net>
2021-06-11 16:41:26 +0900

    revision.h

--- kernel/yuza/revision.h
+++ kernel/yuza/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "06 June 2021"
-#define BUILD_TIME "19:08:26"
+#define BUILD_DATE "11 June 2021"
+#define BUILD_TIME "16:36:57"
 #define BUILD_SYSTEM "msvc"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 11458
+#define REVISION_BUILD 11459
 
 #endif //!_REVISION_H_
