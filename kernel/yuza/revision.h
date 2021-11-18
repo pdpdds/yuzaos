aca08c8d856f33d97faa2e4979d90b61df299596
Juhang Park <juhang3@daum.net>
2021-11-18 16:30:18 +0900

    grep project
    grep off_t fix

--- kernel/yuza/revision.h
+++ kernel/yuza/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "23 August 2021"
-#define BUILD_TIME "14:49:49"
+#define BUILD_DATE "18 November 2021"
+#define BUILD_TIME "16:22:36"
 #define BUILD_SYSTEM "msvc"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 11564
+#define REVISION_BUILD 11565
 
 #endif //!_REVISION_H_
