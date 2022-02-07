609915511f80e146505a5140fe02115dea62fc67
Juhang Park <juhang3@daum.net>
2022-02-07 21:23:20 +0900

    refactoring

--- sample/console/grep/main.cpp
+++ sample/console/grep/main.cpp
@@ -5,7 +5,5 @@ extern "C" int grep_main(int argc, char* argv[]);
 int main(int argc, char** argv)
 {
 	GUIConsoleFramework framework;
-	return framework.Run(argc, argv, grep_main);
-
-	return 0;
+	return framework.Run(argc, argv, grep_main);	
 }
