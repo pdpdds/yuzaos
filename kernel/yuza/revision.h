da2fbf58b039297d506489d48ba2c125b6dfcc1a
Juhang Park <juhang3@daum.net>
2023-04-03 20:02:17 +0900

    refactoring

--- kernel/yuza/revision.h
+++ kernel/yuza/revision.h
@@ -3,12 +3,12 @@
 #ifndef _REVISION_H_
 #define _REVISION_H_
 
-#define BUILD_DATE "18 November 2021"
-#define BUILD_TIME "16:22:36"
+#define BUILD_DATE "18 October 2022"
+#define BUILD_TIME "19:43:43"
 #define BUILD_SYSTEM "msvc"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 11565
+#define REVISION_BUILD 11566
 
 #endif //!_REVISION_H_
