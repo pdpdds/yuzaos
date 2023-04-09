c0df20c36225cd0987bbc2dce4e8a158f6038b44
Juhang Park <juhang3@daum.net>
2023-04-09 16:47:34 +0900

    visual studio 2022 update

--- kernel/yuza/revision.h
+++ kernel/yuza/revision.h
@@ -4,11 +4,11 @@
 #define _REVISION_H_
 
 #define BUILD_DATE "09 April 2023"
-#define BUILD_TIME "16:13:53"
+#define BUILD_TIME "16:41:23"
 #define BUILD_SYSTEM "msvc"
 
 #define REVISION_MAJOR 0
 #define REVISION_MINOR 4
-#define REVISION_BUILD 11567
+#define REVISION_BUILD 11571
 
 #endif //!_REVISION_H_
